56e3aab448ad19a341820e63bb5d88295b91f0b5
duyvu997 <ngocduy0997@gmail.com>
2022-02-15 14:37:39 +0700

    add tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Layout from './components/Layout';
 import reportWebVitals from './reportWebVitals';
+import './styles/output.css'
+import './styles/input.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
